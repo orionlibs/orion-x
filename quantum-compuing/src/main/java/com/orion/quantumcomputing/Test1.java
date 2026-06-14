fcef04c5f50b5830ca7184c55ea898731ef889f1
Dimitrios Efthymiou <dimitrios.efthymiou@yapily.com>
2026-06-15 00:36:05 +0100

    1

--- quantum-compuing/src/main/java/com/orion/quantumcomputing/Test1.java
+++ quantum-compuing/src/main/java/com/orion/quantumcomputing/Test1.java
@@ -2,10 +2,11 @@ package com.orion.quantumcomputing;
 
 import org.redfx.strange.algorithm.Classic;
 
-public class Hello
+public class Test1
 {
     static void main()
     {
         int randomBit = Classic.randomBit();
+        System.out.println(randomBit);
     }
 }
