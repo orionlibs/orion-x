28619606b887cd463dfee6fbcd230c5991ef9358
Dimitrios Efthymiou <dimitrios.efthymiou@yapily.com>
2026-06-22 12:23:18 +0100

    1

--- artificial-intelligence/agent-cli/src/main/java/com/orion/ai/agent/cli/command/prompt/RunPromptCommand.java
+++ artificial-intelligence/agent-cli/src/main/java/com/orion/ai/agent/cli/command/prompt/RunPromptCommand.java
@@ -3,8 +3,8 @@ package com.orion.ai.agent.cli.command.prompt;
 import com.orion.ai.agent.cli.Agent;
 import com.orion.ai.agent.cli.configuration.OrionConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.core.command.annotation.Arguments;
 import org.springframework.shell.core.command.annotation.Command;
-import org.springframework.shell.core.command.annotation.Option;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,12 +17,13 @@ public class RunPromptCommand
 
 
     @Command(name = "/p", description = "Run an agent prompt")
-    public String runPrompt(@Option(longName = "prompt", shortName = 'p') String prompt)
+    public String runPrompt(@Arguments String[] words)
     {
+        String prompt = String.join(" ", words);
         OrionConfiguration.Openrouter.Api api = config.getOpenrouter().getApi();
         OrionConfiguration.Openrouter.Ai ai = config.getOpenrouter().getAi();
+        //System.out.println("running model " + ai.getModelId());
         String response = agent.prompt(api.getKey(), api.getBaseUrl(), ai.getModelId(), prompt);
-        System.out.println("running model " + ai.getModelId());
         return response;
     }
 }
