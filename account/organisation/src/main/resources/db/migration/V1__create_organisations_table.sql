c422cead114372b30732296d9d3e71468055bab1
Dimitrios Efthymiou <dimitrios.efthymiou@yapily.com>
2026-08-28 11:53:32 +0100

    1

--- account/organisation/src/main/resources/db/migration/V1__create_organisations_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE organisations
-(
-    user_id    UUID                     NOT NULL PRIMARY KEY,
-    username   VARCHAR(255)             NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
