f596e142b6faa4a721b0b16b3e87e348f553127e
Dimitrios Efthymiou <dimitrios.efthymiou@yapily.com>
2026-08-28 13:56:56 +0100

    1

--- engineering/digitaltwin/webapp/app/public/assets/js/things/things.js
+++ engineering/digitaltwin/webapp/app/public/assets/js/things/things.js
@@ -8,14 +8,30 @@ const maxReconnectAttempts = 1000000;
 const reconnectDelay = 10000;
 let reconnectTimer = null;
 let stompClient = null;
+let thingsTable;
 
 
 $(document).ready(function()
 {
+    orionCommon.makeGetAJAXCall('/api/things/summaries', thingsPage.loadThingsTable);
     stompClient = orionCommon.connectToWebsocket('/websocket', '/topic/alarms', reconnectAttempts, maxReconnectAttempts, reconnectDelay, reconnectTimer, orionCommon.handleAlarm);
 });
 
 
 let thingsPage =
 {
+    loadThingsTable : function(response)
+    {
+        response.things.forEach(thing => {
+            let tableBodyHTML = '<tr>';
+            tableBodyHTML += '<td class="align-middle">' + thing.name + '</td>';
+            tableBodyHTML += '<td class="align-middle">' + thing.description + '</td>';
+            tableBodyHTML += '<td class="align-middle">' + thing.createdAt + '</td>';
+            tableBodyHTML += '<td class="align-middle">' + thing.updatedAt + '</td>';
+            tableBodyHTML += '</tr>';
+            $("#things-table-body").append(tableBodyHTML);
+        });
+
+        thingsTable = orionCommon.renderDataTable('things-table');
+    }
 };
